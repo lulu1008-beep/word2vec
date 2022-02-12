dec038981ea67c1c09df42ed11bbf0a5f9e173ec
lulu <74212805+lulu1008-beep@users.noreply.github.com>
2022-02-12 00:29:19 -0500

    Add files via upload

---
+++ top100.py
@@ -0,0 +1,40 @@
+from gensim.models import KeyedVectors
+
+#todo
+model = KeyedVectors.load_word2vec_format("./Crown_vector.bin", encoding='utf8', binary=True)
+def top100(wordlist):
+    similar_words=[]
+    for word in wordlist:
+        if word in model.wv.vocab:
+            similar_words.append(model.wv.most_similar(positive=word, topn=100))
+    return similar_words
+
+words_w = ['unkind','unfriendly']
+words_c = ['incompetence','incompetent']
+top100(words_w)
+print(top100(words_w))
+
+
+#todo: 2 parts
+with open("./top100_Crown_negW.txt", 'w', encoding='utf8') as text1:
+    for item in top100(words_w):
+        for similar in item:
+            #text1.write("%s\n" % similar)
+            text1.write(str(similar) +'\n')
+    #print(len(str(similar)))
+
+
+'''
+import nltk
+from nltk.corpus import wordnet
+nltk.download('wordnet')
+synonyms=[]
+antonyms=[]
+for syn in wordnet.synsets("warmth"):
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+print(set(synonyms))
+print(set(antonyms))
+'''
