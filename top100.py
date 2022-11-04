3cc813cf72434f972d03dc107930c4137e877b9d
lulu <74212805+lulu1008-beep@users.noreply.github.com>
2022-11-03 23:58:22 -0400

    Update top100.py

--- top100.py
+++ top100.py
@@ -7,20 +7,32 @@ def top100(wordlist):
     for word in wordlist:
         if word in model.wv.vocab:
             similar_words.append(model.wv.most_similar(positive=word, topn=100))
-    return similar_words
+    final_dict = {}
+
+   # de-duplicate words
+    for i in range(len(similar_words)):
+        for word_list in similar_words[i]:
+            if word_list[0] not in final_dict.keys():
+                final_dict[word_list[0]] = word_list[1]
+            else:
+                if word_list[1] > final_dict[word_list[0]]:
+                    final_dict[word_list[0]] = word_list[1]
+
+    deduplicated_list = list(final_dict.items())
+    return deduplicated_list
 
 words_w = ['unkind','unfriendly']
 words_c = ['incompetence','incompetent']
 top100(words_w)
 print(top100(words_w))
+# print(len(top100(words_w))
 
 
 #todo: 2 parts
 with open("./top100_Crown_negW.txt", 'w', encoding='utf8') as text1:
     for item in top100(words_w):
         for similar in item:
-            #text1.write("%s\n" % similar)
             text1.write(str(similar) +'\n')
-    #print(len(str(similar)))
+    # print(len(str(similar)))
 
    
